ea95d3ef4fa6f14713019dbdebc8e6d85d2cd6b3
emilmv <emilnm@code.edu.az>
2024-02-17 00:15:57 +0400

    Task 2 added

--- Hometask_Array/Program.cs
+++ Hometask_Array/Program.cs
@@ -1,11 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 #region Task1
-int[] arr = { 1, 3, 5, 7, 8, 6, 5, 3, 5, 6, 3, 1, 2, 4 };
-int sum = 0;
-for (int i = 0; i < arr.Length; i++)
-{
-    sum += arr[i];
-}
-Console.WriteLine(sum);
+//int[] arr = { 1, 3, 5, 7, 8, 6, 5, 3, 5, 6, 3, 1, 2, 4 };
+//int sum = 0;
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    sum += arr[i];
+//}
+//Console.WriteLine(sum);
+#endregion
+#region Task2
+//int[] arr = { 1, 3, 5, 10, 7, 9, 20, 30, 50, 70, 999 };
+//int i = 0;
+
+//for (int a = 0; a < arr.Length; a++)
+//{
+//    for (int b = 0; b < arr.Length - 1; b++)
+//    {
+//        if (arr[b] > arr[b + 1])
+//        {
+//            i = arr[b + 1];
+//            arr[b + 1] = arr[b];
+//            arr[b] = i;
+//        }
+//    }
+//}
+//Console.WriteLine(arr[arr.Length - 1]);
 #endregion
